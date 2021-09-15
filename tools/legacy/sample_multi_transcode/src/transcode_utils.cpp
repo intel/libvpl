43c3736339638601356d08681b7806f90ad91d5e
Jeff McAllister <jeffrey.mcallister@intel.com>
2021-09-15 13:50:04 -0700

    Fix SMT raw frame input and lookahead

--- tools/legacy/sample_multi_transcode/src/transcode_utils.cpp
+++ tools/legacy/sample_multi_transcode/src/transcode_utils.cpp
@@ -2428,7 +2428,6 @@ mfxStatus CmdProcessor::ParseParamsForOneSession(mfxU32 argc, msdk_char* argv[])
         }
         else if (0 == msdk_strcmp(argv[i], MSDK_STRING("-lad"))) {
             VAL_CHECK(i + 1 == argc, i, argv[i]);
-            InputParams.nRateControlMethod = MFX_RATECONTROL_LA;
             i++;
             if (MFX_ERR_NONE != msdk_opt_read(argv[i], InputParams.nLADepth)) {
                 PrintError(MSDK_STRING("look ahead depth \"%s\" is invalid"), argv[i]);
@@ -2793,6 +2792,10 @@ mfxStatus CmdProcessor::VerifyAndCorrectInputParams(TranscodingSample::sInputPar
         PrintError(MSDK_STRING(
             "Unsupported value of -lad parameter, must be in range [1,100] or 0 for automatic selection"));
         return MFX_ERR_UNSUPPORTED;
+    }
+
+    if (!InputParams.nRateControlMethod && InputParams.nLADepth) {
+        InputParams.nRateControlMethod = MFX_RATECONTROL_LA;
     }
 
     if ((InputParams.nMaxSliceSize) && !(InputParams.libType & MFX_IMPL_HARDWARE_ANY)) {
