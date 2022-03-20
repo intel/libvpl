7d8c6ef9333b1e12ea7f26b9ba5fd1aa67757eca
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-20 21:56:39 +0800

    Delete doxygen directory

--- doxygen/assets/custom.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-$(window).on('load', function() {
-    $('#side-nav').resizable( "destroy" );
-    $('#nav-btn').on('click', function() { $('#side-nav').css('display', 'flex') });
-});
