8f23387484adfc5ec52adba51796a15631b221a6
Ying Guo <ying2.guo@intel.com>
2022-05-20 16:58:23 -0700

    Fix a lint issue with a long expression

--- tools/legacy/sample_encode/src/pipeline_encode.cpp
+++ tools/legacy/sample_encode/src/pipeline_encode.cpp
@@ -957,10 +957,11 @@ mfxStatus CEncodingPipeline::AllocFrames() {
     MSDK_CHECK_STATUS(sts, "QueryIOSurf (for encoder) failed");
 
     if (!m_pmfxVPP) {
-        if std::max(EncRequest.NumFrameSuggested, m_nPerfOpt) < m_mfxEncParams.AsyncDepth :
-            EncRequest.NumFrameMin = EncRequest.NumFrameSuggested = m_mfxEncParams.AsyncDepth :
-            EncRequest.NumFrameMin = EncRequest.NumFrameSuggested =
-                std::max(EncRequest.NumFrameSuggested, m_nPerfOpt);
+        if std
+            ::max(EncRequest.NumFrameSuggested, m_nPerfOpt) < m_mfxEncParams.AsyncDepth
+                    : EncRequest.NumFrameMin                       = EncRequest.NumFrameSuggested =
+                m_mfxEncParams.AsyncDepth : EncRequest.NumFrameMin = EncRequest.NumFrameSuggested =
+                    std::max(EncRequest.NumFrameSuggested, m_nPerfOpt);
     }
 
     if (EncRequest.NumFrameSuggested < m_mfxEncParams.AsyncDepth)
